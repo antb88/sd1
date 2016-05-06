1c2c5a815e686acf8712f3456d737e2f3471354c
Anton <antb88@gmail.com>
2016-05-06 21:47:43 +0300

    added MakefileParserTest

--- makefile-app/src/test/java/MakefileParserTest.java
+++ makefile-app/src/test/java/MakefileParserTest.java
@@ -5,23 +5,23 @@ import cs.technion.ac.il.sd.ExternalCompiler;
 import cs.technion.ac.il.sd.app.Compilable;
 import cs.technion.ac.il.sd.app.MakefileParser;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import org.mockito.Mockito;
 
 import java.io.File;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.when;
 
 /**
- * Created by ant on 5/6/16.
+ * Tests fpr {@link MakefileParser}
  */
 public class MakefileParserTest {
     private final Injector injector = Guice.createInjector(new AbstractModule() {
         @Override
         protected void configure() {
-            // will be replaced with a real implementation in staff tests
             bind(ExternalCompiler.class).toInstance(Mockito.mock(ExternalCompiler.class));
         }
     });
@@ -39,15 +39,16 @@ public class MakefileParserTest {
         return $.getCompilable(name).get();
     }
 
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(5);
+
     @Test
     public void smallContainsAll() {
         parseFile("small");
-        Set<String> t = $.getCompilables().stream()
-                .map(c -> c.getName())
-                .collect(Collectors.toSet());
-        Assert.assertTrue("should contain main", t.contains("main"));
-        Assert.assertTrue("should contain f1", t.contains("f1"));
-        Assert.assertTrue("should contain exacly 2", t.size() == 2);
+        Set<Compilable> all = $.getCompilables();
+        Assert.assertTrue("should contain main", all.contains(find("main")));
+        Assert.assertTrue("should contain f1", all.contains(find("f1")));
+        Assert.assertTrue("should contain exactly 2", all.size() == 2);
     }
 
     @Test
@@ -60,10 +61,10 @@ public class MakefileParserTest {
     @Test
     public void smallDependencies() {
         parseFile("small");
-        Set<Compilable> deps1 = $.getDependantsOf(find("f1"));
-        Set<Compilable> depsMain = $.getDependantsOf(find("main"));
-        Assert.assertTrue("f1 does not have dependencies", deps1.size() == 0);
-        Assert.assertTrue("main should depend only on f1", depsMain.contains(find("f1")) && depsMain.size() == 1);
+        Set<Compilable> dOf1 = $.getDependantsOf(find("f1"));
+        Set<Compilable> dOfMain = $.getDependantsOf(find("main"));
+        Assert.assertTrue("f1 does not have dependencies", dOf1.size() == 0);
+        Assert.assertTrue("main should depend only on f1", dOfMain.contains(find("f1")) && dOfMain.size() == 1);
     }
 
     @Test
